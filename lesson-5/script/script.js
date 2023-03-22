958926a0b57a73bd89f77d835cd53022a145d3e3
Erubamigodstime <erubamigodstime43@gmail.com>
2023-03-23 00:24:19 +0100

    book of mormon

---
+++ lesson-5/script/script.js
@@ -0,0 +1,23 @@
+const list =document.querySelector("ul");
+const input =document.querySelector("input");
+const button = document.querySelector("button");
+
+function myFunc() {
+    const inputVal = input.value;
+    input.value = '';
+    const childList = document.createElement('li');
+    const span = document.createElement('span');
+    const Btn = document.createElement('button');
+
+    childList.appendChild(span);
+    span.textContent = inputVal;
+    childList.appendChild(Btn);    
+    Btn.textContent = 'x'
+    Btn.style.fontSize  = "30px";
+    Btn.style.color = "#582F0E"    
+    list.appendChild(childList);
+    Btn.addEventListener('click', () => {list.removeChild(childList); });
+    input.focus();
+    
+};
+button.addEventListener('click', myFunc);
