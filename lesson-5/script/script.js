aee196abdc1d8c3ca5d031e239a14c0a5bdb8b5a
Erubamigodstime <erubamigodstime43@gmail.com>
2023-03-23 09:54:55 +0100

    input edited

--- lesson-5/script/script.js
+++ lesson-5/script/script.js
@@ -4,22 +4,26 @@ const button = document.querySelector("button");
 
 button.addEventListener('click', function () {        
     const inputVal = input.value;
-    if (inputVal != "") {
-        input.value = ""    
+    if (inputVal != "") {         
         const childList = document.createElement('li');
         const span = document.createElement('span');
         const Btn = document.createElement('button');
         childList.appendChild(span);
         span.textContent = inputVal;
         childList.appendChild(Btn);    
-        Btn.innerText = "❌"
+        Btn.innerText = "❌";
         Btn.style.fontSize  = "25px";
         Btn.style.color = "#582F0E"    
         list.appendChild(childList);
-        Btn.addEventListener('click', () => {list.removeChild(childList); });
-        input.focus();
-        inputVal = "";
+        Btn.addEventListener('click', () => {list.removeChild(childList); });        
+    	
+	}
+     else {    
+        alert("Please enter a valid chapter");
+        
     }
+    input.value = '';
+    input.focus();
     
     
   
