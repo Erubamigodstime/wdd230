0fdf7ae4ac292565005e23506f1285654be5a18f
Erubamigodstime <erubamigodstime43@gmail.com>
2023-03-23 02:04:11 +0100

    input and normalise edited

--- lesson-5/script/script.js
+++ lesson-5/script/script.js
@@ -2,22 +2,25 @@ const list =document.querySelector("ul");
 const input =document.querySelector("input");
 const button = document.querySelector("button");
 
-function myFunc() {
+button.addEventListener('click', function () {        
     const inputVal = input.value;
-    input.value = '';
-    const childList = document.createElement('li');
-    const span = document.createElement('span');
-    const Btn = document.createElement('button');
-
-    childList.appendChild(span);
-    span.textContent = inputVal;
-    childList.appendChild(Btn);    
-    Btn.innerHTML = '<strong> x </strong>'
-    Btn.style.fontSize  = "30px";
-    Btn.style.color = "#582F0E"    
-    list.appendChild(childList);
-    Btn.addEventListener('click', () => {list.removeChild(childList); });
-    input.focus();
+    if (inputVal != "") {
+        input.value = ""    
+        const childList = document.createElement('li');
+        const span = document.createElement('span');
+        const Btn = document.createElement('button');
+        childList.appendChild(span);
+        span.textContent = inputVal;
+        childList.appendChild(Btn);    
+        Btn.innerText = "❌"
+        Btn.style.fontSize  = "25px";
+        Btn.style.color = "#582F0E"    
+        list.appendChild(childList);
+        Btn.addEventListener('click', () => {list.removeChild(childList); });
+        input.focus();
+        inputVal = "";
+    }
+    
+    
   
-};
-button.addEventListener('click', myFunc);
+});
