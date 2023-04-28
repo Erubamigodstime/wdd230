a64a6a25993bfdddde37184f25fb290d37203dc9
Erubamigodstime <erubamigodstime43@gmail.com>
2023-04-28 02:24:28 +0100

    design princinple page edited

---
+++ lesson-2/scripts/script.js
@@ -0,0 +1,4 @@
+
+  const year = new Date().getFullYear();  
+  const last_updated_element = document.querySelector("#currentdate");
+  last_updated_element.innerHTML = `Last Updated: ${document.lastModified}`;
