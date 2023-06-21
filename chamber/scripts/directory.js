935802df4de59d7a5e48dad3aca4667b7f565eff
Erubamigodstime <erubamigodstime43@gmail.com>
2023-06-21 14:04:15 +0100

    dir edited

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -19,30 +19,30 @@ function displayCompanies(companies){
         let sect = document.createElement('section');
         sect.classList.add('patners','main-patners');
         let fig = document.createElement('figure');
-        fig.classList.add('pat-img','new');
-        let name = document.createElement('h3');
+        fig.classList.add('pat-img','patners-img');
+        let name = document.createElement('h3');    
         let industry = document.createElement('p');
         let line = document.createElement('hr');
         let location = document.createElement('p');
         let domain = document.createElement('p');        
         let image = document.createElement('img');
 
-        name.textContent = `${company.name}`;
+        name.innerHTML = ` <strong class="name" ><strong/>${company.name}`;
         industry.innerHTML = ` <strong > Industry <strong/>:  ${company.industry} `;        
         location.innerHTML = `<strong > Business Location<strong/>: ${company.location} `;
         domain.innerHTML = `<strong > Domain  <strong/>:  ${company.domain} `;
         image.setAttribute( 'src', company.imageurl);
         image.setAttribute( 'alt', `image of ${company.name} }`);
-        image.setAttribute( 'width', '200');
-        image.setAttribute( 'height', '100');
+        image.setAttribute( 'width', '250');
+        image.setAttribute( 'height', '150');
         image.setAttribute( 'loading', 'lazy');
 
         fig.appendChild(image);
 
         sect.appendChild(name);
         sect.appendChild(fig);
-        sect.appendChild(industry);
         sect.appendChild(line);
+        sect.appendChild(industry);        
         sect.appendChild(location);
         sect.appendChild(domain);        
         
